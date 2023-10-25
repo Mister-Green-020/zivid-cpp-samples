fa19dbaf967e1ddd5f57dcd5ef090e494172d9d9
Satja Sivcev <s.satjas@gmail.com>
2023-10-25 13:59:44 +0200

    Add measurements

--- source/Camera/Advanced/CaptureHalconViaZivid/CaptureHalconViaZivid.cpp
+++ source/Camera/Advanced/CaptureHalconViaZivid/CaptureHalconViaZivid.cpp
@@ -9,6 +9,8 @@ Capture a point cloud, with colors, using Zivid SDK, transform it to a Halcon po
 #include <chrono>
 #include <iostream>
 
+using namespace std::chrono;
+
 void savePointCloud(const HalconCpp::HObjectModel3D &model, const std::string &fileName)
 {
     model.WriteObjectModel3d(
@@ -20,12 +22,15 @@ void savePointCloud(const HalconCpp::HObjectModel3D &model, const std::string &f
 
 HalconCpp::HObjectModel3D zividToHalconPointCloud(const Zivid::PointCloud &pointCloud)
 {
+    auto t0 = steady_clock::now();
     const auto width = pointCloud.width();
     const auto height = pointCloud.height();
 
     const auto pointsXYZ = pointCloud.copyPointsXYZ();
     const auto colorsRGBA = pointCloud.copyColorsRGBA();
     const auto normalsXYZ = pointCloud.copyNormalsXYZ();
+
+    auto t1 = steady_clock::now();
 
     int numberOfValidPoints =
         std::count_if(pointsXYZ.data(), pointsXYZ.data() + pointsXYZ.size(), [](const Zivid::PointXYZ &point) {
@@ -86,13 +91,19 @@ HalconCpp::HObjectModel3D zividToHalconPointCloud(const Zivid::PointCloud &point
         }
     }
 
-    std::cout << "Constructing ObjectModel3D based on XYZ data" << std::endl;
+     auto t2 = steady_clock::now();
+
+    //std::cout << "Constructing ObjectModel3D based on XYZ data" << std::endl;
     HalconCpp::HObjectModel3D objectModel3D(tuplePointsX, tuplePointsY, tuplePointsZ);
 
-    std::cout << "Mapping ObjectModel3D data" << std::endl;
+    auto t3 = steady_clock::now();
+
+    //std::cout << "Mapping ObjectModel3D data" << std::endl;
     HalconCpp::SetObjectModel3dAttribMod(objectModel3D, "xyz_mapping", "object", tupleXYZMapping);
 
-    std::cout << "Adding normals to ObjectModel3D" << std::endl;
+    auto t4 = steady_clock::now();
+
+    //std::cout << "Adding normals to ObjectModel3D" << std::endl;
     HalconCpp::HTuple normalsAttribNames, normalsAttribValues;
     normalsAttribNames.Append("point_normal_x");
     normalsAttribNames.Append("point_normal_y");
@@ -104,18 +115,45 @@ HalconCpp::HObjectModel3D zividToHalconPointCloud(const Zivid::PointCloud &point
 
     HalconCpp::SetObjectModel3dAttribMod(objectModel3D, normalsAttribNames, "points", normalsAttribValues);
 
-    std::cout << "Adding RGB to ObjectModel3D" << std::endl;
+    auto t5 = steady_clock::now();
+
+    //std::cout << "Adding RGB to ObjectModel3D" << std::endl;
     HalconCpp::SetObjectModel3dAttribMod(objectModel3D, "red", "points", tupleColorsR);
     HalconCpp::SetObjectModel3dAttribMod(objectModel3D, "green", "points", tupleColorsG);
     HalconCpp::SetObjectModel3dAttribMod(objectModel3D, "blue", "points", tupleColorsB);
+
+    auto t6 = steady_clock::now();
+
+    /*std::cout << " " << std::endl;
+
+    std::cout << duration_cast<milliseconds>(t1 - t0).count() << " ms - t0 - "
+              << "copy points, colors, and normals" << std::endl;
+    std::cout << duration_cast<milliseconds>(t2 - t1).count() << " ms - t1 - "
+              << "Initializing HTuples" << std::endl;
+    std::cout << duration_cast<milliseconds>(t3 - t2).count() << " ms - t2 - "
+              << "Constructing ObjectModel3D based on XYZ data: " << std::endl;
+    std::cout << duration_cast<milliseconds>(t4 - t3).count() << " ms - t3 - "
+              << "Mapping ObjectModel3D data" << std::endl;
+    std::cout << duration_cast<milliseconds>(t5 - t4).count() << " ms - t4 - "
+              << "Adding normals to ObjectModel3D" << std::endl;
+    std::cout << duration_cast<milliseconds>(t6 - t5).count() << " ms - t5 - "
+              << "Adding RGB to ObjectModel3D" << std::endl;*/
+    std::cout << duration_cast<milliseconds>(t6 - t1).count()
+              << " ms - t1+t2+t3+t4+t5 - "
+              << "Finalize Halcon" << std::endl;
 
     return objectModel3D;
 }
 
 int main()
 {
-    try
-    {
+    try {
+        auto t01 = steady_clock::now();
+        HalconCpp::HSystem::GetSystem("is_license_valid");
+        auto t02 = steady_clock::now();
+        std::cout << duration_cast<milliseconds>(t02 - t01).count() << " ms - "
+              << "Halcon license check" << std::endl;
+
         std::cout << "Connecting to camera" << std::endl;
         Zivid::Application zivid;
         auto camera = zivid.connectCamera();
@@ -130,16 +168,19 @@ int main()
                              Zivid::Settings::Processing::Filters::Smoothing::Gaussian::Enabled::yes,
                              Zivid::Settings::Processing::Filters::Smoothing::Gaussian::Sigma{ 1.5 } };
 
-        std::cout << "Capturing frame" << std::endl;
-        const auto frame = camera.capture(settings);
-        const auto zividPointCloud = frame.pointCloud();
+        for (int i = 0; i < 25; i++)
+        { 
+            // std::cout << "Capturing frame" << std::endl;
+            const auto frame = camera.capture(settings);
+            const auto zividPointCloud = frame.pointCloud();
 
-        std::cout << "Converting to Halcon point cloud" << std::endl;
-        const auto halconPointCloud = zividToHalconPointCloud(zividPointCloud);
+            // std::cout << "Converting to Halcon point cloud" << std::endl;
+            const auto halconPointCloud = zividToHalconPointCloud(zividPointCloud);
 
-        const auto pointCloudFile = "Zivid3D.ply";
-        std::cout << "Saving point cloud to file: " << pointCloudFile << std::endl;
-        savePointCloud(halconPointCloud, pointCloudFile);
+            const auto pointCloudFile = "Zivid3D.ply";
+            // std::cout << "Saving point cloud to file: " << pointCloudFile << std::endl;
+            savePointCloud(halconPointCloud, pointCloudFile);
+        }
     }
 
     catch(HalconCpp::HException &except)
